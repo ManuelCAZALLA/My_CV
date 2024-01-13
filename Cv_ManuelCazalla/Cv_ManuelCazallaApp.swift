6e8f8d83ab30d1d30a579dab0340fe757df732cd
Manuel Cazalla <manuelcm23@hotmail.com>
2024-01-13 10:57:25 +0100

    Creo Carpetas y prueba de vista principal

---
+++ Cv_ManuelCazalla/Cv_ManuelCazallaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cv_ManuelCazallaApp.swift
+//  Cv_ManuelCazalla
+//
+//  Created by Manuel Cazalla Colmenero on 9/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Cv_ManuelCazallaApp: App {
+    var body: some Scene {
+        WindowGroup {
+           SplashView()
+        }
+    }
+}
